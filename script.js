bb42b75fbeabbcc479b80a6c7813e14253ace37f
Chijioke <wisestcodes@gmail.com>
2023-07-15 04:45:00 -0400

    remove cookies notice

--- script.js
+++ script.js
@@ -28,3 +28,31 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//////////////////////////////////////////////////////////////////////////////////
+console.log(document);
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// create and insert elements
+const cookieNotice = document.createElement('div');
+cookieNotice.classList.add('cookie-message');
+cookieNotice.innerHTML =
+  'This site uses cookies for improved perfomance and analytics.<button class="btn btn--close-cookie">Got it</button>';
+
+header.prepend(cookieNotice);
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', () => cookieNotice.remove());
