70bc8d7d420422d87be5bcc842cfda56bbfb0b6b
Chijioke <wisestcodes@gmail.com>
2023-07-15 22:40:43 -0400

    event propagation

--- script.js
+++ script.js
@@ -65,3 +65,31 @@ btnScrollTo.addEventListener('click', function (e) {
 
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+console.log(randomColor());
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log('LINK');
+  this.style.backgroundColor = randomColor();
+  console.log('Link: ', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log('LINK');
+  this.style.backgroundColor = randomColor();
+  console.log('Container: ', e.target, e.currentTarget);
+});
+
+document.querySelector('nav').addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log('LINK');
+  this.style.backgroundColor = randomColor();
+  console.log('Nav: ', e.target, e.currentTarget);
+});
